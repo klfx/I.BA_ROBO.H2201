1b7951fffd3db3c96eda0106f494eae574cde7b2
Flavio Kluser <flavio.kluser@stud.hslu.ch>
2022-12-06 09:25:53 +0100

    init H1

--- pepper/H1/main.py
+++ pepper/H1/main.py
@@ -1,1 +1,16 @@
-print("test")
+from pepper_robots import PepperConfiguration, Robot, PepperNames
+
+config = PepperConfiguration(PepperNames.Ale)
+pepper = Robot(config)
+
+tts = pepper.ALAnimatedSpeech  # text to speech service
+
+tts.say("Test")
+
+
+success = pepper.ALMotion.moveTo(0.5, 0, 0)
+
+if not success:
+    tts.say("Ohh, I could not move. Maybe there is an obstacle in front of me. Or my brake is not released")
+if success:
+    tts.say("Yeah,A I've reached my destination")
---
+++ pepper/H1/readme.md
@@ -0,0 +1,7 @@
+# HSLU ROBO H1
+## Prerequisites
+Copy dependencies from robotics_lab into this directory
+https://gitlab.enterpriselab.ch/pepper/robotics_lab
+
+* naoqi_python_wrapper (dir)
+* pepper_robots.py
