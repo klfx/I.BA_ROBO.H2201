9e21c727f9585c2627363b2efffb90dd07e08976
Flavio Kluser <flavio.kluser@stud.hslu.ch>
2022-12-12 11:28:49 +0100

    add api call and list return

--- pepper/H1/sockets/client.py
+++ pepper/H1/sockets/client.py
@@ -4,7 +4,12 @@
 
 import socket
 import sys
+import time
+import random
+from speech_recognition import SpeechRecognition
+
 from pepper_robots import PepperConfiguration, Robot, PepperNames
+from naoqi_python_wrapper.ALAutonomousLife import ALAutonomousLife
 from take_picture import pepper_spies
 
 config = PepperConfiguration(PepperNames.Ale)
@@ -17,16 +22,39 @@ print("starting python client with python version:")
 print(sys.version)
 
 tts = pepper.ALTextToSpeech
+pepper.ALAutonomousLife.setState("solitary")
+pepper.ALRobotPosture.goToPosture("StandZero", 1.0)
 tts.setLanguage("English")
 tts.setVolume(0.6)
 tts.say("Let's play a game of I spy with my little eye")
 
 tts.say("I'l start")
+#pepper.ALMotion.moveTo(-0.5, 0, 0)
 pepper_spies(pepper)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 s.sendall(b'Picture taken')
 data = s.recv(1024)
+print('original data:'+data)
+#data = data.decode('utf-8')
 
-print('Received', repr(data))
+data = data.strip('][').split(', ')
+
+itemlist = []
+for i in data:
+    i = i.replace("'", "")
+    pepper.ALTextToSpeech.say(i)
+    itemlist.append(i)
+
+selecteditem = random.choice(itemlist)
+
+#sp = SpeechRecognition(pepper,itemlist,speech_callback)
+
+#for i in itemlist:
+    #print(i)
+    #tts.say(i)
+    #time.sleep(1)
+
+#print(repr(data))
+#print(data)
